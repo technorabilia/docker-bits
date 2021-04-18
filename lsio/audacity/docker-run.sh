33dc90a7469852a9c291ced5ca55d55099865f70
technorabilia <simon@technorabilia.com>
2021-04-18 00:56:28 +0000

    Workflow update

---
+++ lsio/audacity/docker-run.sh
@@ -0,0 +1,13 @@
+# [Audacity]({{ project_url }}) is an easy-to-use, multi-track audio editor and
+# recorder. Developed by a group of volunteers as open source.
+
+source ./.env
+docker run -d \
+  --name=audacity \
+  -e PUID=${PUID:-1024} `# for GroupID` \
+  -e PGID=${PGID:-100} `# for UserID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
+  -p 3000:3000 `# Audacity desktop gui.` \
+  -v ${BASEDIR:-/volume1/docker}/audacity/config:/config `# Users home directory in the container, stores program settings and images` \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/audacity
---
+++ lsio/endlessh/docker-run.sh
@@ -0,0 +1,19 @@
+# Endlessh Endlessh is an SSH tarpit that very slowly sends an endless, random
+# SSH banner. It keeps SSH clients locked up for hours or even days at a time.
+# The purpose is to put your real SSH server on another port and then let the
+# script kiddies get stuck in this tarpit instead of bothering a real server.
+
+source ./.env
+docker run -d \
+  --name=endlessh \
+  -e PUID=${PUID:-1024} `# for GroupID` \
+  -e PGID=${PGID:-100} `# for UserID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
+  -e MSDELAY=10000 `# optional` `# The endless banner is sent one line at a time. This is the delay in milliseconds between individual lines.` \
+  -e MAXLINES=32 `# optional` `# The length of each line is randomized. This controls the maximum length of each line. Shorter lines may keep clients on for longer if they give up after a certain number of bytes.` \
+  -e MAXCLIENTS=4096 `# optional` `# Maximum number of connections to accept at a time. Connections beyond this are not immediately rejected, but will wait in the queue.` \
+  -e LOGFILE=false `# optional` `# By default, the app logs to container log. If this is set to `true`, the log will be output to file under `/config.logs/endlessh` (`/config` needs to be mapped).` \
+  -p 22:2222 `# ssh port` \
+  -v ${BASEDIR:-/volume1/docker}/endlessh/config:/config `# optional` `# Required if `LOGFILE` is set to `true`.` \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/endlessh
