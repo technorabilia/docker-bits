b2f745f51979f88b5e226633ce1015b637163398
technorabilia <simon@technorabilia.com>
2021-04-20 00:55:53 +0000

    Workflow update

---
+++ lsio/filezilla/docker-run.sh
@@ -0,0 +1,14 @@
+# [FIleZilla]({{ project_url }}) Client is a fast and reliable cross-platform
+# FTP, FTPS and SFTP client with lots of useful features and an intuitive
+# graphical user interface.
+
+source ./.env
+docker run -d \
+  --name=filezilla \
+  -e PUID=${PUID:-1024} `# for GroupID` \
+  -e PGID=${PGID:-100} `# for UserID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
+  -p 3000:3000 `# FileZilla desktop gui.` \
+  -v ${BASEDIR:-/volume1/docker}/filezilla/config:/config `# Users home directory in the container, stores local files and settings` \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/filezilla
---
+++ lsio/firefox/docker-run.sh
@@ -0,0 +1,17 @@
+# [Firefox]({{ project_url }}) Browser, also known as Mozilla Firefox or simply
+# Firefox, is a free and open-source web browser developed by the Mozilla
+# Foundation and its subsidiary, the Mozilla Corporation. Firefox uses the Gecko
+# layout engine to render web pages, which implements current and anticipated
+# web standards.
+
+source ./.env
+docker run -d \
+  --name=firefox \
+  -e PUID=${PUID:-1024} `# for GroupID` \
+  -e PGID=${PGID:-100} `# for UserID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
+  -p 3000:3000 `# Firefox desktop gui.` \
+  -v ${BASEDIR:-/volume1/docker}/firefox/config:/config `# Users home directory in the container, stores local files and settings` \
+  --shm-size="1gb" \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/firefox
