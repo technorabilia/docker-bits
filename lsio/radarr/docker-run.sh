0215d38cdf103d8786ba22e128ab72e4174f672c
technorabilia <simon@technorabilia.com>
2021-05-10 07:39:17 +0000

    Workflow update

--- lsio/radarr/docker-run.sh
+++ lsio/radarr/docker-run.sh
@@ -9,7 +9,7 @@ docker run -d \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
   -p 7878:7878 `# The port for the Radarr webinterface` \
   -v ${BASEDIR:-/volume1/docker}/radarr/config:/config `# Database and Radarr configs` \
-  -v ${BASEDIR:-/volume1/docker}/radarr/movies:/movies `# Location of Movie library on disk (See note in Application setup)` \
-  -v ${BASEDIR:-/volume1/docker}/radarr/downloads:/downloads `# Location of download managers output directory (See note in Application setup)` \
+  -v ${BASEDIR:-/volume1/docker}/radarr/movies:/movies `# optional` `# Location of Movie library on disk (See note in Application setup)` \
+  -v ${BASEDIR:-/volume1/docker}/radarr/downloads:/downloads `# optional` `# Location of download managers output directory (See note in Application setup)` \
   --restart unless-stopped \
   ghcr.io/linuxserver/radarr
--- lsio/radarr/run-once.sh
+++ lsio/radarr/run-once.sh
@@ -1,5 +1,5 @@
 ln -s ../docker-env.cfg ./.env
 . ./.env
 mkdir -p ${BASEDIR:-/volume1/docker}/radarr/config
-mkdir -p ${BASEDIR:-/volume1/docker}/radarr/movies
-mkdir -p ${BASEDIR:-/volume1/docker}/radarr/downloads
+mkdir -p ${BASEDIR:-/volume1/docker}/radarr/movies `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/radarr/downloads `#optional`
