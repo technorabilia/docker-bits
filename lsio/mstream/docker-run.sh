44f50756d4208e2ae5a52a1d8ac34e0e1ebe5eba
technorabilia <simon@technorabilia.com>
2021-06-04 01:43:27 +0000

    Workflow update

--- lsio/mstream/docker-run.sh
+++ lsio/mstream/docker-run.sh
@@ -7,9 +7,6 @@ docker run -d \
   --name=mstream \
   -e PUID=${PUID:-1024} `# for GroupID` \
   -e PGID=${PGID:-100} `# for UserID` \
-  -e USER=admin `# Set username to login` \
-  -e PASSWORD=password `# Set password for user` \
-  -e USE_JSON=true/false `# Run mStream using the config specified at `/config/config.json`, note this will mean user/password is defined in `config.json`` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
   -p 3000:3000 `# The port for the mStream webinterface` \
   -v ${BASEDIR:-/volume1/docker}/mstream/config:/config `# mStream config` \
