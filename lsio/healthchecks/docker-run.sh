8ec9ff7594211d055fb85ef7e9628070872972ea
technorabilia <simon@technorabilia.com>
2021-08-08 00:47:10 +0000

    Workflow update

--- lsio/healthchecks/docker-run.sh
+++ lsio/healthchecks/docker-run.sh
@@ -18,7 +18,8 @@ docker run -d \
   -e ALLOWED_HOSTS=<ALLOWED_HOSTS> `# array of valid hostnames for the server `["test.com","test2.com"]` or `"*"`` \
   -e SUPERUSER_EMAIL=<SUPERUSER_EMAIL> `# Superuser email` \
   -e SUPERUSER_PASSWORD=<SUPERUSER_PASSWORD> `# Superuser password` \
-  -e REGENERATE_SETTINGS=True/False `# Defaults to False. Set to true to always override the `local_settings.py` file with values from environment variables. Do not set to True if you have made manual modifications to this file.` \
+  -e REGENERATE_SETTINGS=True/False `# optional` `# Defaults to False. Set to true to always override the `local_settings.py` file with values from environment variables. Do not set to True if you have made manual modifications to this file.` \
+  -e SITE_LOGO_URL=<SITE_LOGO_URL> `# optional` `# Custom site logo URL` \
   -p 8000:8000 `# will map the container's port 8000 to port 8000 on the host` \
   -v ${BASEDIR:-/volume1/docker}/healthchecks/config:/config `# database and healthchecks config directory volume mapping` \
   --restart unless-stopped \
