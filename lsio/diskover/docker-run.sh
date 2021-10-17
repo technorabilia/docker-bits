622507b08361f06490f8b5ed39bec0bb85644312
technorabilia <simon@technorabilia.com>
2021-10-17 00:51:24 +0000

    Workflow update

--- lsio/diskover/docker-run.sh
+++ lsio/diskover/docker-run.sh
@@ -1,5 +1,5 @@
-# [Diskover](https://github.com/shirosaidev/diskover) is a file system crawler
-# and disk space usage software that uses Elasticsearch to index and manage data
+# [Diskover](https://github.com/diskoverdata/diskover-community) is an open
+# source file system indexer that uses Elasticsearch to index and manage data
 # across heterogeneous storage systems.
 
 source ./.env
@@ -8,20 +8,11 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for GroupID` \
   -e PGID=${PGID:-100} `# for UserID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
-  -e REDIS_HOST=redis `# Redis host (optional)` \
-  -e REDIS_PORT=6379 `# Redis port (optional)` \
   -e ES_HOST=elasticsearch `# ElasticSearch host (optional)` \
   -e ES_PORT=9200 `# ElasticSearch port (optional)` \
   -e ES_USER=elastic `# ElasticSearch username (optional)` \
   -e ES_PASS=changeme `# ElasticSearch password (optional)` \
-  -e INDEX_NAME=diskover- `# Index name prefix (optional)` \
-  -e DISKOVER_OPTS= `# Optional arguments to pass to the diskover crawler (optional)` \
-  -e WORKER_OPTS= `# Optional argumens to pass to the diskover bots launcher (optional)` \
-  -e RUN_ON_START=true `# Initiate a crawl every time the container is started (optional)` \
-  -e USE_CRON=true `# Run a crawl on as a cron job (optional)` \
   -p 80:80 `# diskover Web UI` \
-  -p 9181:9181 `# rq-dashboard web UI` \
-  -p 9999:9999 `# diskover socket server` \
   -v ${BASEDIR:-/volume1/docker}/diskover/config:/config `# Persistent config files` \
   -v ${BASEDIR:-/volume1/docker}/diskover/data:/data `# Default mount point to crawl` \
   --restart unless-stopped \
