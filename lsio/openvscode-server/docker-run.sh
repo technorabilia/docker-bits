48345a36be7ff8d681c8a94835b6d0dc135fc032
technorabilia <simon@technorabilia.com>
2021-11-30 01:05:45 +0000

    Workflow update

---
+++ lsio/openvscode-server/docker-run.sh
@@ -0,0 +1,16 @@
+# [Openvscode-server](https://github.com/gitpod-io/openvscode-server) provides a
+# version of VS Code that runs a server on a remote machine and allows access
+# through a modern web browser.
+
+source ./.env
+docker run -d \
+  --name=openvscode-server \
+  -e PUID=${PUID:-1024} `# for GroupID` \
+  -e PGID=${PGID:-100} `# for UserID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
+  -e CONNECTION_TOKEN=supersecrettoken `# optional` `# Optional security token for accessing the Web UI.` \
+  -e CONNECTION_SECRET= `# optional` `# Optional path to a file inside the container that contains the security token for accessing the Web UI (ie. `/path/to/file`). Overrides `CONNECTION_TOKEN`.` \
+  -p 3000:3000 `# Web UI port.` \
+  -v ${BASEDIR:-/volume1/docker}/openvscode-server/config:/config `# Contains all relevant configuration files.` \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/openvscode-server
