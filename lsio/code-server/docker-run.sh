61d087088e5eb0a305fa951e9e69edfc3f064e29
technorabilia <simon@technorabilia.com>
2021-12-07 01:10:20 +0000

    Workflow update

--- lsio/code-server/docker-run.sh
+++ lsio/code-server/docker-run.sh
@@ -17,6 +17,7 @@ docker run -d \
   -e SUDO_PASSWORD=password `# optional` `# If this optional variable is set, user will have sudo access in the code-server terminal with the specified password.` \
   -e SUDO_PASSWORD_HASH= `# optional` `# Optionally set sudo password via hash (takes priority over `SUDO_PASSWORD` var). Format is `$type$salt$hashed`.` \
   -e PROXY_DOMAIN=code-server.my.domain `# optional` `# If this optional variable is set, this domain will be proxied for subdomain proxying. See [Documentation](https://github.com/cdr/code-server/blob/master/docs/FAQ.md#sub-domains)` \
+  -e DEFAULT_WORKSPACE=/config/workspace `# optional` `# If this optional variable is set, code-server will open this directory by default` \
   -p 8443:8443 `# web gui` \
   -v ${BASEDIR:-/volume1/docker}/code-server/config:/config `# Contains all relevant configuration files.` \
   --restart unless-stopped \
