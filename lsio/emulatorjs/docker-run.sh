600ed405a0eb84fa82814f427054634a5157fce2
technorabilia <simon@technorabilia.com>
2022-01-15 00:36:25 +0000

    Workflow update

--- lsio/emulatorjs/docker-run.sh
+++ lsio/emulatorjs/docker-run.sh
@@ -12,6 +12,7 @@ docker run -d \
   -p 3000:3000 `# Rom/artwork management interface, used to generate/manage config files and download artwork` \
   -p 80:80 `# Emulation frontend containing static web files used to browse and launch games` \
   -p 4001:4001 `# optional` `# IPFS peering port, if you want to participate in the P2P network to distribute frontend artwork please forward this to the Internet` \
+  -v ${BASEDIR:-/volume1/docker}/emulatorjs/config:/config `# Path to store user profiles` \
   -v ${BASEDIR:-/volume1/docker}/emulatorjs/data:/data `# Path to store roms/artwork` \
   --restart unless-stopped \
   ghcr.io/linuxserver/emulatorjs
