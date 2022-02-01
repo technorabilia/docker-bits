0602cf332be04696999a97f752b01198893dde0a
technorabilia <simon@technorabilia.com>
2022-02-01 00:36:41 +0000

    Workflow update

--- lsio/doplarr/docker-run.sh
+++ lsio/doplarr/docker-run.sh
@@ -8,22 +8,22 @@ docker run -d \
   -e PGID=${PGID:-100} `# for UserID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
   -e DISCORD__TOKEN=<discord__token> `# Specify your discord bot token.` \
-  -e OVERSEERR__API= `# Specify your Overseerr API key. Leave blank if using Radarr/Sonarr.` \
+  -e OVERSEERR__API=<overseerr__api> `# Specify your Overseerr API key. Leave blank if using Radarr/Sonarr.` \
   -e OVERSEERR__URL=http://localhost:5055 `# Specify your Overseerr URL. Leave blank if using Radarr/Sonarr.` \
-  -e RADARR__API= `# Specify your Radarr API key. Leave blank if using Overseerr.` \
+  -e RADARR__API=<radarr__api> `# Specify your Radarr API key. Leave blank if using Overseerr.` \
   -e RADARR__URL=http://localhost:7878 `# Specify your Radarr URL. Leave blank if using Overseerr.` \
-  -e SONARR__API= `# Specify your Sonarr API key. Leave blank if using Overseerr.` \
+  -e SONARR__API=<sonarr__api> `# Specify your Sonarr API key. Leave blank if using Overseerr.` \
   -e SONARR__URL=http://localhost:8989 `# Specify your Sonarr URL. Leave blank if using Overseerr.` \
   -e DISCORD__MAX_RESULTS=25 `# optional` `# Sets the maximum size of the search results selection` \
-  -e DISCORD__ROLE_ID= `# optional` `# The discord role id for users of the bot (omitting this lets everyone on the server use the bot)` \
+  -e DISCORD__ROLE_ID=<not_set_by_default> `# optional` `# The discord role id for users of the bot (omitting this lets everyone on the server use the bot)` \
   -e DISCORD__REQUESTED_MSG_STYLE=:plain `# optional` `# Sets the style of the request alert message. One of `:plain` `:embed` `:none`` \
-  -e SONARR__QUALITY_PROFILE= `# optional` `# The name of the quality profile to use by default for Sonarr` \
-  -e RADARR__QUALITY_PROFILE= `# optional` `# The name of the quality profile to use by default for Radarr` \
-  -e SONARR__LANGUAGE_PROFILE= `# optional` `# The name of the language profile to use by default for Radarr` \
-  -e OVERSEERR__DEFAULT_ID= `# optional` `# The Overseerr user id to use by default if there is no associated discord account for the requester` \
+  -e SONARR__QUALITY_PROFILE=<not_set_by_default> `# optional` `# The name of the quality profile to use by default for Sonarr` \
+  -e RADARR__QUALITY_PROFILE=<not_set_by_default> `# optional` `# The name of the quality profile to use by default for Radarr` \
+  -e SONARR__LANGUAGE_PROFILE=<not_set_by_default> `# optional` `# The name of the language profile to use by default for Radarr` \
+  -e OVERSEERR__DEFAULT_ID=<not_set_by_default> `# optional` `# The Overseerr user id to use by default if there is no associated discord account for the requester` \
   -e PARTIAL_SEASONS=true `# optional` `# Sets whether users can request partial seasons.` \
   -e LOG_LEVEL=:info `# optional` `# The log level for the logging backend. This can be changed for debugging purposes. One of trace `:debug` `:info` `:warn` `:error` `:fatal` `:report`` \
-  -e JAVA_OPTS= `# optional` `# For passing additional java options.` \
+  -e JAVA_OPTS=<not_set_by_default> `# optional` `# For passing additional java options.` \
   -v ${BASEDIR:-/volume1/docker}/doplarr/config:/config `# Contains all relevant configuration files.` \
   --restart unless-stopped \
   ghcr.io/linuxserver/doplarr
---
+++ lsio/plex-meta-manager/docker-run.sh
@@ -0,0 +1,20 @@
+# [Plex-meta-manager](https://github.com/meisnate12/Plex-Meta-Manager) is a
+# Python 3 script that can be continuously run using YAML configuration files to
+# update on a schedule the metadata of the movies, shows, and collections in
+# your libraries as well as automatically build collections based on various
+# methods all detailed in the wiki.
+
+source ./.env
+docker run -d \
+  --name=plex-meta-manager \
+  -e PUID=${PUID:-1024} `# for GroupID` \
+  -e PGID=${PGID:-100} `# for UserID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
+  -e PMM_CONFIG=/config/config.yml `# optional` `# Specify a custom config file to use.` \
+  -e PMM_TIME=03:00 `# optional` `# Comma-separated list of times to update each day. Format: `HH:MM`.` \
+  -e PMM_RUN=False `# optional` `# Set to `True` to run without the scheduler.` \
+  -e PMM_TEST=False `# optional` `# Set to `True` to run in debug mode with only collections that have `test: true`.` \
+  -e PMM_NO_MISSING=False `# optional` `# Set to `True` to run without any of the missing movie/show functions.` \
+  -v ${BASEDIR:-/volume1/docker}/plex-meta-manager/config:/config `# Local path for plex-meta-manager config files.` \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/plex-meta-manager
