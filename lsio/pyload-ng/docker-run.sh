30bf2c0e268f95ecafed58942e1ac5c383aac7cb
technorabilia <simon@technorabilia.com>
2022-02-04 00:35:34 +0000

    Workflow update

--- lsio/pyload-ng/docker-run.sh
+++ lsio/pyload-ng/docker-run.sh
@@ -9,6 +9,7 @@ docker run -d \
   -e PGID=${PGID:-100} `# for UserID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
   -p 8000:8000 `# Allows HTTP access to the application` \
+  -p 9666:9666 `# optional` `# Click'n'Load port.` \
   -v ${BASEDIR:-/volume1/docker}/pyload-ng/config:/config `# pyLoad Configuration and files database` \
   -v ${BASEDIR:-/volume1/docker}/pyload-ng/downloads:/downloads `# Destination of pyLoad downloads` \
   --restart unless-stopped \
