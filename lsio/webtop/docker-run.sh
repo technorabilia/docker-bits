9af9c9ae3b51d769f4c9d2cd01b5c6a04169e2a6
technorabilia <simon@technorabilia.com>
2022-02-07 00:35:51 +0000

    Workflow update

--- lsio/webtop/docker-run.sh
+++ lsio/webtop/docker-run.sh
@@ -1,15 +1,15 @@
-# [Webtop](https://github.com/linuxserver/docker-webtop) - Alpine, Ubuntu,
-# Fedora, and Arch based containers containing full desktop environments in
-# officially supported flavors accessible via any modern web browser.
+# [Webtop](https://github.com/linuxserver/gclient) - Alpine, Ubuntu, Fedora, and
+# Arch based containers containing full desktop environments in officially
+# supported flavors accessible via any modern web browser.
 
 source ./.env
 docker run -d \
   --name=webtop \
-  --privileged `# optional` \
   -e PUID=${PUID:-1024} `# for GroupID` \
   -e PGID=${PGID:-100} `# for UserID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
   -e SUBFOLDER=/ `# optional` `# Specify a subfolder to use with reverse proxies, IE `/subfolder/`` \
+  -e KEYBOARD=en-us-qwerty `# optional` `# See the keyboard layouts section for more information and options.` \
   -p 3000:3000 `# Web Desktop GUI` \
   -v ${BASEDIR:-/volume1/docker}/webtop/config:/config `# abc users home directory` \
   -v /var/run/docker.sock:/var/run/docker.sock `# optional` `# Docker Socket on the system, if you want to use Docker in the container` \
