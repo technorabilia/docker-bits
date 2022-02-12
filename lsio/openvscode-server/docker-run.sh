422886f0bd2a88f60c3b74024c1b8962e26324e0
technorabilia <simon@technorabilia.com>
2022-02-12 00:39:37 +0000

    Workflow update

--- lsio/openvscode-server/docker-run.sh
+++ lsio/openvscode-server/docker-run.sh
@@ -8,7 +8,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for GroupID` \
   -e PGID=${PGID:-100} `# for UserID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
-  -e CONNECTION_TOKEN=supersecrettoken `# optional` `# Optional security token for accessing the Web UI.` \
+  -e CONNECTION_TOKEN= `# optional` `# Optional security token for accessing the Web UI (ie. `supersecrettoken`).` \
   -e CONNECTION_SECRET= `# optional` `# Optional path to a file inside the container that contains the security token for accessing the Web UI (ie. `/path/to/file`). Overrides `CONNECTION_TOKEN`.` \
   -e SUDO_PASSWORD=password `# optional` `# If this optional variable is set, user will have sudo access in the openvscode-server terminal with the specified password.` \
   -e SUDO_PASSWORD_HASH= `# optional` `# Optionally set sudo password via hash (takes priority over `SUDO_PASSWORD` var). Format is `$type$salt$hashed`.` \
