fefc0270b5d42dfb7ae010c9546e9008c6cef2fc
technorabilia <simon@technorabilia.com>
2022-02-18 00:41:36 +0000

    Workflow update

--- lsio/sqlitebrowser/docker-run.sh
+++ lsio/sqlitebrowser/docker-run.sh
@@ -8,7 +8,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for GroupID` \
   -e PGID=${PGID:-100} `# for UserID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
-  -p 3000:3000 `# Sqlitebrowser desktop gui, only use this if you are not using host mode and sniffing Docker network traffic.` \
+  -p 3000:3000 `# Sqlitebrowser desktop gui.` \
   -v ${BASEDIR:-/volume1/docker}/sqlitebrowser/config:/config `# Users home directory in the container, stores program settings and potentially dump files.` \
   --restart unless-stopped \
   ghcr.io/linuxserver/sqlitebrowser
