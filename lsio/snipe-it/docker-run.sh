f3e310af942c1df3fa996c8da38b05922887c1fe
technorabilia <simon@technorabilia.com>
2022-03-05 00:44:53 +0000

    Workflow update

--- lsio/snipe-it/docker-run.sh
+++ lsio/snipe-it/docker-run.sh
@@ -8,12 +8,13 @@ docker run -d \
   --name=snipe-it \
   -e PUID=${PUID:-1024} `# for GroupID` \
   -e PGID=${PGID:-100} `# for UserID` \
-  -e NGINX_APP_URL=<hostname or ip> `# Hostname or IP and port if applicable IE <ip or hostname>:8080` \
+  -e APP_URL=http://localhost:8080 `# Hostname or IP and port if applicable, be sure to define https/http` \
   -e MYSQL_PORT_3306_TCP_ADDR=<mysql host> `# Mysql hostname or IP to use` \
   -e MYSQL_PORT_3306_TCP_PORT=<mysql port> `# Mysql port to use` \
   -e MYSQL_DATABASE=<mysql database> `# Mysql database to use` \
   -e MYSQL_USER=<mysql pass> `# Mysql user to use` \
   -e MYSQL_PASSWORD=changeme `# Mysql password to use` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
   -p 8080:80 `# Snipe-IT Web UI` \
   -v ${BASEDIR:-/volume1/docker}/snipe-it/config:/config `# Contains your config files and data storage for Snipe-IT` \
   --restart unless-stopped \
