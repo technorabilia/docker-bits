8d46f30b473e501bbd4e7c8aa9c326240f38f6e0
technorabilia <simon@technorabilia.com>
2022-03-08 00:44:50 +0000

    Workflow update

--- lsio/rdesktop/docker-run.sh
+++ lsio/rdesktop/docker-run.sh
@@ -1,16 +1,17 @@
-# [Rdesktop](http://xrdp.org/) - Ubuntu based containers containing full desktop
-# environments in officially supported flavors accessible via RDP.
+# [Rdesktop](http://xrdp.org/) - Containers containing full desktop environments
+# in many popular flavors for Alpine, Ubuntu, Arch, and Fedora accessible via
+# RDP.
 
 source ./.env
 docker run -d \
   --name=rdesktop \
-  --privileged `# optional` \
   -e PUID=${PUID:-1024} `# for GroupID` \
   -e PGID=${PGID:-100} `# for UserID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
   -p 3389:3389 `# RDP access port` \
   -v /var/run/docker.sock:/var/run/docker.sock `# optional` `# Docker Socket on the system, if you want to use Docker in the container` \
   -v ${BASEDIR:-/volume1/docker}/rdesktop/config:/config `# optional` `# abc users home directory` \
+  --device /dev/dri:/dev/dri `# optional` `# Add this for GL support (Linux hosts only)` \
   --shm-size="1gb" `# optional` \
   --restart unless-stopped \
   ghcr.io/linuxserver/rdesktop
