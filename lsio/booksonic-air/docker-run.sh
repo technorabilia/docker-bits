e9ef186b664326f16b3b82bcb97524b0971cfc4e
technorabilia <simon@technorabilia.com>
2022-04-22 00:57:09 +0000

    Workflow update

--- lsio/booksonic-air/docker-run.sh
+++ lsio/booksonic-air/docker-run.sh
@@ -1,8 +1,8 @@
 # [Booksonic-air](http://booksonic.org) is a platform for accessing the
-# audiobooks you own wherever you are. At the moment the platform consists of
+# audiobooks you own wherever you are. At the moment the platform consists of: *
 # Booksonic Air - A server for streaming your audiobooks, successor to the
-# original Booksonic server and based on Airsonic. Booksonic App - An DSub based
-# Android app for connection to Booksonic-Air servers. .
+# original Booksonic server and based on Airsonic. * Booksonic App - An DSub
+# based Android app for connection to Booksonic-Air servers.
 
 source ./.env
 docker run -d \
