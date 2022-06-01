162100ca9bbf01d24156d079c528c9cce63cb90d
technorabilia <simon@technorabilia.com>
2022-06-01 01:05:29 +0000

    Workflow update

--- lsio/babybuddy/docker-run.sh
+++ lsio/babybuddy/docker-run.sh
@@ -5,6 +5,8 @@
 source ./.env
 docker run -d \
   --name=babybuddy \
+  -e PUID=${PUID:-1024} `# for UserID` \
+  -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
   -e CSRF_TRUSTED_ORIGINS=http://127.0.0.1:8000,https://babybuddy.domain.com `# Add any address you'd like to access babybuddy at (comma separated, no spaces)` \
   -p 8000:8000 `# the port for the web ui` \
