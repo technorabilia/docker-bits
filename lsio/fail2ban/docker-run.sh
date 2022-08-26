a82ad6f6ad41de5d7ea9c84843834b2e7505b87c
technorabilia <simon@technorabilia.com>
2022-08-26 01:06:26 +0000

    Workflow update

--- lsio/fail2ban/docker-run.sh
+++ lsio/fail2ban/docker-run.sh
@@ -13,27 +13,22 @@ docker run -d \
   -p 80:80 `# Application WebUI` \
   -v ${BASEDIR:-/volume1/docker}/fail2ban/config:/config `# Contains all relevant configuration files.` \
   -v ${BASEDIR:-/volume1/docker}/fail2ban/var/log:ro:/var/log:ro `# Host logs. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/airsonic/airsonic.log:ro:/remotelogs/airsonic/airsonic.log:ro `# optional` `# Optional path to airsonic log file. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/airsonic:ro:/remotelogs/airsonic:ro `# optional` `# Optional path to airsonic log folder. Mounted as Read Only.` \
   -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/apache2:ro:/remotelogs/apache2:ro `# optional` `# Optional path to apache2 log folder. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/audit/audit.log:ro:/remotelogs/audit/audit.log:ro `# optional` `# Optional path to auditd log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/authelia/authelia.log:ro:/remotelogs/authelia/authelia.log:ro `# optional` `# Optional path to authelia log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/emby/embyserver.txt:ro:/remotelogs/emby/embyserver.txt:ro `# optional` `# Optional path to emby log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/exim/mainlog:ro:/remotelogs/exim/mainlog:ro `# optional` `# Optional path to exim log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/filebrowser/filebrowser.log:ro:/remotelogs/filebrowser/filebrowser.log:ro `# optional` `# Optional path to filebrowser log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/gitea/gitea.log:ro:/remotelogs/gitea/gitea.log:ro `# optional` `# Optional path to gitea log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/homeassistant/home-assistant.log:ro:/remotelogs/homeassistant/home-assistant.log:ro `# optional` `# Optional path to homeassistant log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/lighttpd/error.log:ro:/remotelogs/lighttpd/error.log:ro `# optional` `# Optional path to lighttpd error log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nextcloud/nextcloud.log:ro:/remotelogs/nextcloud/nextcloud.log:ro `# optional` `# Optional path to nextcloud log file. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/authelia:ro:/remotelogs/authelia:ro `# optional` `# Optional path to authelia log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/emby:ro:/remotelogs/emby:ro `# optional` `# Optional path to emby log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/filebrowser:ro:/remotelogs/filebrowser:ro `# optional` `# Optional path to filebrowser log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/homeassistant:ro:/remotelogs/homeassistant:ro `# optional` `# Optional path to homeassistant log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/lighttpd:ro:/remotelogs/lighttpd:ro `# optional` `# Optional path to lighttpd log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nextcloud:ro:/remotelogs/nextcloud:ro `# optional` `# Optional path to nextcloud log folder. Mounted as Read Only.` \
   -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nginx:ro:/remotelogs/nginx:ro `# optional` `# Optional path to nginx log folder. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nzbget/nzbget.log:ro:/remotelogs/nzbget/nzbget.log:ro `# optional` `# Optional path to nzbget log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/overseerr/overseerr.log:ro:/remotelogs/overseerr/overseerr.log:ro `# optional` `# Optional path to overseerr log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/prowlarr/prowlarr.txt:ro:/remotelogs/prowlarr/prowlarr.txt:ro `# optional` `# Optional path to prowlarr log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/radarr/radarr.txt:ro:/remotelogs/radarr/radarr.txt:ro `# optional` `# Optional path to radarr log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/roundcube/errors:ro:/remotelogs/roundcube/errors:ro `# optional` `# Optional path to roundcube error log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/sabnzbd/sabnzbd.log:ro:/remotelogs/sabnzbd/sabnzbd.log:ro `# optional` `# Optional path to sabnzbd log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/sonarr/sonarr.txt:ro:/remotelogs/sonarr/sonarr.txt:ro `# optional` `# Optional path to sonarr log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/unificontroller/server.log:ro:/remotelogs/unificontroller/server.log:ro `# optional` `# Optional path to unificontroller server log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/vaultwarden/vaultwarden.log:ro:/remotelogs/vaultwarden/vaultwarden.log:ro `# optional` `# Optional path to vaultwarden log file. Mounted as Read Only.` \
-  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/vsftpd/vsftpd.log:ro:/remotelogs/vsftpd/vsftpd.log:ro `# optional` `# Optional path to vsftpd log file. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nzbget:ro:/remotelogs/nzbget:ro `# optional` `# Optional path to nzbget log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/overseerr:ro:/remotelogs/overseerr:ro `# optional` `# Optional path to overseerr log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/prowlarr:ro:/remotelogs/prowlarr:ro `# optional` `# Optional path to prowlarr log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/radarr:ro:/remotelogs/radarr:ro `# optional` `# Optional path to radarr log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/sabnzbd:ro:/remotelogs/sabnzbd:ro `# optional` `# Optional path to sabnzbd log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/sonarr:ro:/remotelogs/sonarr:ro `# optional` `# Optional path to sonarr log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/unificontroller:ro:/remotelogs/unificontroller:ro `# optional` `# Optional path to unificontroller log folder. Mounted as Read Only.` \
+  -v ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/vaultwarden:ro:/remotelogs/vaultwarden:ro `# optional` `# Optional path to vaultwarden log folder. Mounted as Read Only.` \
   --restart unless-stopped \
   ghcr.io/linuxserver/fail2ban
--- lsio/fail2ban/run-once.sh
+++ lsio/fail2ban/run-once.sh
@@ -2,25 +2,20 @@ ln -s ../docker-env.cfg ./.env
 . ./.env
 mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/config
 mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/var/log:ro
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/airsonic/airsonic.log:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/airsonic:ro `#optional`
 mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/apache2:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/audit/audit.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/authelia/authelia.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/emby/embyserver.txt:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/exim/mainlog:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/filebrowser/filebrowser.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/gitea/gitea.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/homeassistant/home-assistant.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/lighttpd/error.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nextcloud/nextcloud.log:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/authelia:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/emby:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/filebrowser:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/homeassistant:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/lighttpd:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nextcloud:ro `#optional`
 mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nginx:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nzbget/nzbget.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/overseerr/overseerr.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/prowlarr/prowlarr.txt:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/radarr/radarr.txt:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/roundcube/errors:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/sabnzbd/sabnzbd.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/sonarr/sonarr.txt:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/unificontroller/server.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/vaultwarden/vaultwarden.log:ro `#optional`
-mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/vsftpd/vsftpd.log:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/nzbget:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/overseerr:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/prowlarr:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/radarr:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/sabnzbd:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/sonarr:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/unificontroller:ro `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/fail2ban/remotelogs/vaultwarden:ro `#optional`
