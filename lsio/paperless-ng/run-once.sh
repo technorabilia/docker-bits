43c8c78606173fc6a7924f233fee77a4c2f64900
technorabilia <simon@technorabilia.com>
2022-09-26 01:12:25 +0000

    Workflow update

--- lsio/paperless-ng/run-once.sh
+++
@@ -1,4 +0,0 @@
-ln -s ../docker-env.cfg ./.env
-. ./.env
-mkdir -p ${BASEDIR:-/volume1/docker}/paperless-ng/config
-mkdir -p ${BASEDIR:-/volume1/docker}/paperless-ng/data
--- lsio/paperless-ngx/run-once.sh
+++
@@ -1,4 +0,0 @@
-ln -s ../docker-env.cfg ./.env
-. ./.env
-mkdir -p ${BASEDIR:-/volume1/docker}/paperless-ngx/config
-mkdir -p ${BASEDIR:-/volume1/docker}/paperless-ngx/data
