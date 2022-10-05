f895a0e9ff113333fd71a4756110e020f596c160
technorabilia <simon@technorabilia.com>
2022-10-05 01:16:35 +0000

    Workflow update

--- lsio/projectsend/docker-run.sh
+++ lsio/projectsend/docker-run.sh
@@ -9,7 +9,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
-  -e MAX_UPLOAD=<5000> `# To set maximum upload size (in MB), default if unset is 5000.` \
+  -e MAX_UPLOAD=5000 `# To set maximum upload size (in MB), default if unset is 5000.` \
   -p 80:80 `# WebUI` \
   -v ${BASEDIR:-/volume1/docker}/projectsend/config:/config `# Where to store projectsend config files.` \
   -v ${BASEDIR:-/volume1/docker}/projectsend/data:/data `# Where to store files to share.` \
