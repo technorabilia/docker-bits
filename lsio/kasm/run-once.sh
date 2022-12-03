d25e24c4082c43b432372c4be3f659aaf1810eac
technorabilia <simon@technorabilia.com>
2022-12-03 00:49:22 +0000

    Workflow update

--- lsio/kasm/run-once.sh
+++ lsio/kasm/run-once.sh
@@ -2,3 +2,5 @@ ln -s ../docker-env.cfg ./.env
 . ./.env
 mkdir -p ${BASEDIR:-/volume1/docker}/kasm/opt
 mkdir -p ${BASEDIR:-/volume1/docker}/kasm/profiles `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/kasm/dev/input `#optional`
+mkdir -p ${BASEDIR:-/volume1/docker}/kasm/run/udev/data `#optional`
