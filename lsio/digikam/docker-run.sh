9430117f434926ba3dac2d641841553841f72850
technorabilia <simon@technorabilia.com>
2023-01-28 00:56:42 +0000

    Workflow update

--- lsio/digikam/docker-run.sh
+++ lsio/digikam/docker-run.sh
@@ -4,7 +4,6 @@
 source ./.env
 docker run -d \
   --name=digikam \
-  --net=host `# Use Host Networking` \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# Specify a timezone to use for example Europe/Amsterdam` \
