a3a02e9f56969d57f2c3af6f8fc0d35a1c32dc22
technorabilia <simon@technorabilia.com>
2023-03-19 01:02:23 +0000

    Workflow update

---
+++ lsio/chromium/docker-run.sh
@@ -0,0 +1,16 @@
+# [Chromium](https://www.chromium.org/chromium-projects/) is an open-source
+# browser project that aims to build a safer, faster, and more stable way for
+# all users to experience the web.
+
+source ./.env
+docker run -d \
+  --name=chromium \
+  -e PUID=${PUID:-1024} `# for UserID` \
+  -e PGID=${PGID:-100} `# for GroupID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -p 3000:3000 `# Chromium desktop gui.` \
+  -p 3001:3001 `# HTTPS Chromium desktop gui.` \
+  -v ${BASEDIR:-/volume1/docker}/chromium/config:/config `# Users home directory in the container, stores local files and settings` \
+  --shm-size="1gb" \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/chromium
