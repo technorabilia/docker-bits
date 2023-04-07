8c8c25ad8121ccf5543e5947007627e798bd45d0
technorabilia <simon@technorabilia.com>
2023-04-07 00:47:31 +0000

    Workflow update

--- lsio/chromium/docker-run.sh
+++ lsio/chromium/docker-run.sh
@@ -8,6 +8,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -e CHROME_CLI=https://www.linuxserver.io/ `# optional` `# Specify one or multiple Chromium CLI flags, this string will be passed to the application in full.` \
   -p 3000:3000 `# Chromium desktop gui.` \
   -p 3001:3001 `# HTTPS Chromium desktop gui.` \
   -v ${BASEDIR:-/volume1/docker}/chromium/config:/config `# Users home directory in the container, stores local files and settings` \
