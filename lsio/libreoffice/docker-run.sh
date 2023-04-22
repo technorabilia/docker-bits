674c605588a3bec93fb35008887dbb4d72499aa1
technorabilia <simon@technorabilia.com>
2023-04-22 00:53:15 +0000

    Workflow update

--- lsio/libreoffice/docker-run.sh
+++ lsio/libreoffice/docker-run.sh
@@ -10,7 +10,7 @@ docker run -d \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
   -p 3000:3000 `# LibreOffice desktop gui.` \
-  -p 3000:3000 `# LibreOffice desktop gui HTTPS.` \
+  -p 3001:3001 `# LibreOffice desktop gui HTTPS.` \
   -v ${BASEDIR:-/volume1/docker}/libreoffice/config:/config `# Users home directory in the container, stores program settings and documents` \
   --restart unless-stopped \
   ghcr.io/linuxserver/libreoffice
