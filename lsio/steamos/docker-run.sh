3423802916e92d7b67b7b603c63a0b98c89e1f00
technorabilia <simon@technorabilia.com>
2023-08-13 00:46:31 +0000

    Workflow update

--- lsio/steamos/docker-run.sh
+++ lsio/steamos/docker-run.sh
@@ -13,13 +13,18 @@ docker run -d \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
   -e DRINODE=/dev/dri/renderD128 `# Specify the render device (GPU) for the contianer to use.` \
   -e HOST_IP=192.168.100.10 `# optional` `# Specify the IP of the host, needed for LAN Remote Play.` \
+  -e STARTUP=KDE `# optional` `# KDE to boot into desktop mode, BIGPICTURE to boot into gamescope.` \
+  -e RESOLUTION=1920x1080 `# optional` `# When booting into BIGPICTURE mode the screen resolution will be bound to this value.` \
   -p 3000:3000 `# SteamOS desktop gui.` \
   -p 3001:3001 `# HTTPS SteamOS desktop gui.` \
   -p 27031-27036:27031-27036/udp `# optional` `# Steam Remote Play Ports (UDP).` \
   -p 27031-27036:27031-27036 `# optional` `# Steam Remote Play Ports (TCP).` \
+  -p 47984-47990:47984-47990:47984-47990:47984-47990/tcp `# optional` `# Sunshine Ports (TCP).` \
+  -p 48010:48010:48010:48010 `# optional` `# Sunshine Ports (TCP).` \
+  -p 47998-48000:47998-48000:47998-48000:47998-48000/udp `# optional` `# Sunshine Ports (UDP).` \
   -v ${BASEDIR:-/volume1/docker}/steamos/config:/config `# Users home directory in the container, stores all files and games.` \
-  -v /dev/input:/dev/input `# optional` `# Optional for gamepad support.` \
-  -v /run/udev/data:/run/udev/data `# optional` `# Optional for gamepad support.` \
+  -v /dev/input:/dev/input `# optional` `# Optional for gamepad support. *Only working for Steam Remote Play` \
+  -v /run/udev/data:/run/udev/data `# optional` `# Optional for gamepad support. *Only working for Steam Remote Play` \
   --device /dev/dri:/dev/dri `# Video card passthrough to Steam.` \
   --shm-size="1gb" \
   --restart unless-stopped \
