b93a99e525a5016d9bf402af4cf30666284a6ae2
technorabilia <simon@technorabilia.com>
2023-08-29 01:14:04 +0000

    Workflow update

--- lsio/habridge/docker-run.sh
+++ lsio/habridge/docker-run.sh
@@ -1,9 +1,9 @@
-# [Habridge](http://bwssystems.com/#/habridge) emulates Philips Hue API to other
-# home automation gateways such as an Amazon Echo/Dot Gen 1 (gen 2 has issues
-# discovering ha-bridge) or other systems that support Philips Hue. The Bridge
-# handles basic commands such as 'On', 'Off' and 'brightness' commands of the
-# hue protocol. This bridge can control most devices that have a distinct API.
-# In the cases of systems that require authorization and/or have APIs that
+# [Habridge](https://bwssystems.com/#!/habridge) emulates Philips Hue API to
+# other home automation gateways such as an Amazon Echo/Dot Gen 1 (gen 2 has
+# issues discovering ha-bridge) or other systems that support Philips Hue. The
+# Bridge handles basic commands such as 'On', 'Off' and 'brightness' commands of
+# the hue protocol. This bridge can control most devices that have a distinct
+# API. In the cases of systems that require authorization and/or have APIs that
 # cannot be handled in the current method, a module may need to be built. The
 # Harmony Hub is such a module and so is the Nest module. The Bridge has helpers
 # to build devices for the gateway for the Logitech Harmony Hub, Vera, Vera Lite
