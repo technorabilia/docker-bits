98a80a894ac9508ee9009619d5a35d38d7d3f333
technorabilia <simon@technorabilia.com>
2023-11-15 00:36:22 +0000

    Workflow update

--- lsio/hishtory-server/docker-run.sh
+++ lsio/hishtory-server/docker-run.sh
@@ -9,7 +9,8 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
-  -e HISHTORY_POSTGRES_DB=postgresql://${HISHTORY_DB_USER}:${HISHTORY_DB_PASS}@hishtory-db:5432/hishtory?sslmode=disable `# Postgres DB [Connection URI](https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING). Special characters must be [URL encoded](https://en.wikipedia.org/wiki/URL_encoding).` \
+  -e HISHTORY_POSTGRES_DB=postgresql://${HISHTORY_DB_USER}:${HISHTORY_DB_PASS}@hishtory-db:5432/hishtory?sslmode=disable `# optional` `# Postgres DB [Connection URI](https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING). Special characters must be [URL encoded](https://en.wikipedia.org/wiki/URL_encoding).` \
+  -e HISHTORY_SQLITE_DB=/config/hishtory.db `# optional` `# SQLite database path. Needs to be a mounted volume for persistence. Don't set at the same time as HISHTORY_POSTGRES_DB.` \
   -p 8080:8080 `# API port` \
   --restart unless-stopped \
   ghcr.io/linuxserver/hishtory-server
