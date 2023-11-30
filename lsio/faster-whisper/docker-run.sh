50de70df7265fc21e76c826a7a4e74123f7d3ecc
technorabilia <simon@technorabilia.com>
2023-11-30 00:37:17 +0000

    Workflow update

---
+++ lsio/faster-whisper/docker-run.sh
@@ -0,0 +1,17 @@
+# [Faster-whisper](https://github.com/SYSTRAN/faster-whisper) is a
+# reimplementation of OpenAI's Whisper model using CTranslate2, which is a fast
+# inference engine for Transformer models.
+
+. ./.env
+docker run -d \
+  --name=faster-whisper \
+  -e PUID=${PUID:-1024} `# for UserID` \
+  -e PGID=${PGID:-100} `# for GroupID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -e WHISPER_MODEL=tiny-int8 `# Whisper model that will be used for transcription.` \
+  -e WHISPER_BEAM=1 `# optional` `# Number of candidates to consider simultaneously during transcription.` \
+  -e WHISPER_LANG=en `# optional` `# Language that you will speak to the add-on.` \
+  -p 10300:10300 `# Wyoming connection port.` \
+  -v ${BASEDIR:-/volume1/docker}/faster-whisper/config:/config `# Local path for Whisper config files.` \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/faster-whisper
