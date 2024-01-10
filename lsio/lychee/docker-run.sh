9c76c463b8d4ec09d8a6038c6b1d2896b13bc5a3
technorabilia <simon@technorabilia.com>
2024-01-10 01:23:17 +0000

    Workflow update

--- lsio/lychee/docker-run.sh
+++ lsio/lychee/docker-run.sh
@@ -1,10 +1,7 @@
 # [Lychee](https://lycheeorg.github.io/) is a free photo-management tool, which
 # runs on your server or web-space. Installing is a matter of seconds. Upload,
 # manage and share photos like from a native application. Lychee comes with
-# everything you need and all your photos are stored securely.' ### UPGRADE
-# WARNING Please note that the v4 upgrade process resets ALL password-protected
-# albums. Any albums that were made public with a password will need to be re-
-# secured.
+# everything you need and all your photos are stored securely.'
 
 . ./.env
 docker run -d \
@@ -12,14 +9,17 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
-  -e DB_CONNECTION=mysql `# for specifying the database type` \
-  -e DB_HOST=mariadb `# for specifying the database host` \
-  -e DB_PORT=3306 `# for specifying the database port` \
-  -e DB_USERNAME=lychee `# for specifying the database user` \
-  -e DB_PASSWORD=dbpassword `# for specifying the database password` \
-  -e DB_DATABASE=lychee `# for specifying the database to be used` \
+  -e DB_CONNECTION= `# DB type, from `sqlite`, `mysql`, `pqsql`.` \
+  -e DB_HOST= `# DB server hostname. For `mysql` and `pgsql` only.` \
+  -e DB_PORT= `# DB server port. For `mysql` and `pgsql` only.` \
+  -e DB_USERNAME= `# DB user. For `mysql` and `pgsql` only.` \
+  -e DB_PASSWORD= `# DB password. For `mysql` and `pgsql` only.` \
+  -e DB_DATABASE= `# Path to DB file for `sqlite`. DB name for `mysql` and `pgsql`.` \
+  -e APP_NAME=Lychee `# optional` `# The gallery name.` \
+  -e APP_URL= `# optional` `# The URL you will use to access Lychee including protocol, and port where appropriate.` \
+  -e APP_FORCE_HTTPS= `# optional` `# Set to `true` if running behind an https reverse proxy.` \
   -p 80:80 `# http gui` \
-  -v ${BASEDIR:-/volume1/docker}/lychee/config:/config `# Contains all relevant configuration files.` \
-  -v ${BASEDIR:-/volume1/docker}/lychee/pictures:/pictures `# Where lychee will store uploaded data.` \
+  -v ${BASEDIR:-/volume1/docker}/lychee/config:/config `# Persistent config files.` \
+  -v ${BASEDIR:-/volume1/docker}/lychee/pictures:/pictures `# Where lychee will store uploaded images.` \
   --restart unless-stopped \
   ghcr.io/linuxserver/lychee
