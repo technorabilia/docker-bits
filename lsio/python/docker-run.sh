b9edb8c565550714dfbf7ee7ed49851c72930fac
technorabilia <simon@technorabilia.com>
2024-03-09 00:31:47 +0000

    Workflow update

---
+++ lsio/python/docker-run.sh
@@ -0,0 +1,11 @@
+This container needs special attention. Please check https://hub.docker.com/r/linuxserver/python for details.
+. ./.env
+docker run -d \
+  --name=python \
+  -e PUID=${PUID:-1024} `# for UserID` \
+  -e PGID=${PGID:-100} `# for GroupID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -p 80:80 `# Application WebUI` \
+  -v ${BASEDIR:-/volume1/docker}/python/config:/config `# Configuration files.` \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/python
