808361be7cc90e775354cb24f256a504e170845c
technorabilia <simon@technorabilia.com>
2024-03-11 00:34:16 +0000

    Workflow update

--- lsio/changedetection.io/docker-run.sh
+++ lsio/changedetection.io/docker-run.sh
@@ -8,6 +8,7 @@ docker run -d \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
   -e BASE_URL= `# optional` `# Specify the full URL (including protocol) when running behind a reverse proxy` \
+  -e PLAYWRIGHT_DRIVER_URL= `# optional` `# Specify the full URL to your chrome driver instance. See the [wiki](https://github.com/dgtlmoon/changedetection.io/wiki/Playwright-content-fetcher) for details.` \
   -p 5000:5000 `# WebUI` \
   -v ${BASEDIR:-/volume1/docker}/changedetection.io/config:/config `# Persistent config files` \
   --restart unless-stopped \
