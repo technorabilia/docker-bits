40fdbd5f0ea3624df8c2116bd1ec495a5ee6ee0c
technorabilia <simon@technorabilia.com>
2024-03-23 00:32:01 +0000

    Workflow update

--- lsio/smokeping/docker-run.sh
+++ lsio/smokeping/docker-run.sh
@@ -8,6 +8,9 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -e MASTER_URL=http://<master-host-ip>:80/smokeping/ `# optional` `# Specify the master url to connect to. Used when in slave mode.` \
+  -e SHARED_SECRET=password `# optional` `# Specify the master shared secret for this host. Used when in slave mode.` \
+  -e CACHE_DIR=/tmp `# optional` `# Specify the cache directory for this host. Used when in slave mode.` \
   -p 80:80 `# Allows HTTP access to the internal webserver.` \
   -v ${BASEDIR:-/volume1/docker}/smokeping/config:/config `# Persistent config files` \
   -v ${BASEDIR:-/volume1/docker}/smokeping/data:/data `# Storage location for db and application data (graphs etc)` \
