b2c035fb0a70c2d180927030a518a0cc1df18f19
technorabilia <simon@technorabilia.com>
2024-04-09 00:34:02 +0000

    Workflow update

---
+++ lsio/socket-proxy/docker-run.sh
@@ -0,0 +1,11 @@
+This container needs special attention. Please check https://hub.docker.com/r/linuxserver/socket-proxy for details.
+. ./.env
+docker run -d \
+  --name=socket-proxy \
+  -e PUID=${PUID:-1024} `# for UserID` \
+  -e PGID=${PGID:-100} `# for GroupID` \
+  -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -p 80:80 `# Application WebUI` \
+  -v ${BASEDIR:-/volume1/docker}/socket-proxy/config:/config `# Configuration files.` \
+  --restart unless-stopped \
+  ghcr.io/linuxserver/socket-proxy
