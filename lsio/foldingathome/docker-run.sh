5998afd37b9e2753dcf8a5671873a34c1ff5d497
technorabilia <simon@technorabilia.com>
2024-06-16 00:40:10 +0000

    Workflow update

--- lsio/foldingathome/docker-run.sh
+++ lsio/foldingathome/docker-run.sh
@@ -12,6 +12,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -e CLI_ARGS= `# optional` `# Optionally pass additional cli arguments to `FAHClient` on container start.` \
   -p 7396:7396 `# Folding@home web gui.` \
   -p 36330:36330 `# optional` `# Optional port for connecting remotely via FAHControl app (no password).` \
   -v ${BASEDIR:-/volume1/docker}/foldingathome/config:/config `# Where Folding@home should store its database and config.` \
