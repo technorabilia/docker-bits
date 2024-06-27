ace3264cdebbe57f4ec84dca09a87856616612a9
technorabilia <simon@technorabilia.com>
2024-06-27 00:36:56 +0000

    Workflow update

--- lsio/foldingathome/docker-run.sh
+++ lsio/foldingathome/docker-run.sh
@@ -12,9 +12,11 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
-  -e CLI_ARGS= `# optional` `# Optionally pass additional cli arguments to `FAHClient` on container start.` \
-  -p 7396:7396 `# Folding@home web gui.` \
-  -p 36330:36330 `# optional` `# Optional port for connecting remotely via FAHControl app (no password).` \
+  -e ACCOUNT_TOKEN= `# Register for an account on `https://app.foldingathome.org` and retrieve account token in settings. Required on first start.` \
+  -e MACHINE_NAME= `# Assign a friendly name to this instance (no spaces). Required on first start.` \
+  -e CLI_ARGS= `# optional` `# Optionally pass additional cli arguments to `fah-client` on container start.` \
+  -p 80:80 `# Application WebUI` \
+  -p 7396:7396 `# optional` `# Folding@home web gui (redirects to [https://app.foldingathome.org](https://app.foldingathome.org)).` \
   -v ${BASEDIR:-/volume1/docker}/foldingathome/config:/config `# Where Folding@home should store its database and config.` \
   --restart unless-stopped \
   ghcr.io/linuxserver/foldingathome
