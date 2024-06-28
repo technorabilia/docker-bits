538b8ae76e141566554e3722a303f029e32f2d74
technorabilia <simon@technorabilia.com>
2024-06-28 00:36:59 +0000

    Workflow update

--- lsio/webgrabplus/docker-run.sh
+++ lsio/webgrabplus/docker-run.sh
@@ -9,6 +9,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -p 80:80 `# Application WebUI` \
   -v ${BASEDIR:-/volume1/docker}/webgrabplus/config:/config `# Persistent config files` \
   -v ${BASEDIR:-/volume1/docker}/webgrabplus/data:/data `# Where webgrabplus should store it's data files.` \
   --restart unless-stopped \
