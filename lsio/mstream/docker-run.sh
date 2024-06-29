7980b6ce1c10ad54dd90d990f1fca71a9fb79364
technorabilia <simon@technorabilia.com>
2024-06-29 00:36:47 +0000

    Workflow update

--- lsio/mstream/docker-run.sh
+++ lsio/mstream/docker-run.sh
@@ -8,7 +8,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
-  -p 3000:3000 `# The port for the mStream webinterface` \
+  -p 3000:3000 `# The port for the mStream web interface` \
   -v ${BASEDIR:-/volume1/docker}/mstream/config:/config `# mStream config` \
   -v ${BASEDIR:-/volume1/docker}/mstream/music:/music `# Music location` \
   --restart unless-stopped \
