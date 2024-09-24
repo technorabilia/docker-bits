170a5a5a82fb7f69ab8a27033997ce61b3bd7b40
technorabilia <simon@technorabilia.com>
2024-09-24 00:43:35 +0000

    Workflow update

--- lsio/syslog-ng/docker-run.sh
+++ lsio/syslog-ng/docker-run.sh
@@ -8,6 +8,7 @@ docker run -d \
   -e PUID=${PUID:-1024} `# for UserID` \
   -e PGID=${PGID:-100} `# for GroupID` \
   -e TZ=${TZ:-Europe/Amsterdam} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -e LOG_TO_STDOUT= `# optional` `# If set to `true` container will log to stdout, otherwise will log to `/config/log/`.` \
   -p 514:5514/udp `# Syslog UDP` \
   -p 601:6601/tcp `# Syslog TCP` \
   -p 6514:6514/tcp `# Syslog TLS` \
