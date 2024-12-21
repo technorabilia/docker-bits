e0341656c501c0bb34a5bde6903829522dee324b
technorabilia <simon@technorabilia.com>
2024-12-21 00:44:14 +0000

    Workflow update

--- lsio/synclounge/docker-run.sh
+++ lsio/synclounge/docker-run.sh
@@ -6,5 +6,6 @@ docker run -d \
   --name=synclounge \
   -e AUTH_LIST=plexuser1,plexuser2,email1,machineid1 `# optional` `# If set, only the users defined here and the users of the plex servers defined here will be able to access the server. Use e-mails, plex usernames and/or plex server machine ids, comma separated, no spaces.` \
   -p 8088:8088 `# Web app and server port` \
+  -v ${BASEDIR:-/volume1/docker}/synclounge/config:/config `# Configuration files.` \
   --restart unless-stopped \
   ghcr.io/linuxserver/synclounge
