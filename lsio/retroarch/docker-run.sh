502944ce2b46707816d23c7d93f20f5d8f1309e5
technorabilia <simon@technorabilia.com>
2025-06-09 00:57:00 +0000

    Workflow update

--- lsio/retroarch/docker-run.sh
+++ lsio/retroarch/docker-run.sh
@@ -8,8 +8,8 @@ docker run -d \
   -e PUID=${PUID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the UID to use here.` \
   -e PGID=${PGID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the GID to use here.` \
   -e TZ=${TZ:-Etc/UTC} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
-  -p 3000:3000 `# Retroarch desktop gui HTTP must be proxied.` \
-  -p 3001:3001 `# Retroarch desktop gui HTTPS.` \
+  -p 3000:3000 `# RetroArch desktop gui HTTP must be proxied.` \
+  -p 3001:3001 `# RetroArch desktop gui HTTPS.` \
   -v ${BASEDIR:-/volume1/docker}/retroarch/config:/config `# Users home directory in the container, stores local files and settings` \
   --shm-size="1gb" \
   --restart unless-stopped \
