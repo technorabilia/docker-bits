49e471e0c5cde3509718782cb08d3be0134a1543
technorabilia <simon@technorabilia.com>
2025-07-11 00:55:57 +0000

    Workflow update

--- lsio/sickchill/run-once.sh
+++
@@ -1,5 +0,0 @@
-ln -s ../docker-env.cfg ./.env
-. ./.env
-mkdir -p ${BASEDIR:-/srv/lsio}/sickchill/config
-mkdir -p ${BASEDIR:-/srv/lsio}/sickchill/downloads `# [OPTIONAL]`
-mkdir -p ${BASEDIR:-/srv/lsio}/sickchill/tv `# [OPTIONAL]`
