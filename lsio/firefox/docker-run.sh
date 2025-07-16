f98f8dcb2054bc95998493161a86de9b997876be
technorabilia <simon@technorabilia.com>
2025-07-16 00:57:18 +0000

    Workflow update

--- lsio/firefox/docker-run.sh
+++ lsio/firefox/docker-run.sh
@@ -11,7 +11,7 @@ docker run -d \
   -e PGID=${PGID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the GID to use here.` \
   -e TZ=${TZ:-Etc/UTC} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
   -e FIREFOX_CLI=https://www.linuxserver.io/ `# Specify one or multiple Firefox CLI flags, this string will be passed to the application in full. [OPTIONAL]` \
-  -p 3000:3000 `# Firefox desktop gui.` \
+  -p 3000:3000 `# Firefox desktop gui HTTP, must be proxied.` \
   -p 3001:3001 `# Firefox desktop gui HTTPS.` \
   -v ${BASEDIR:-/srv/lsio}/firefox/config:/config `# Users home directory in the container, stores local files and settings` \
   --shm-size="1gb" \
