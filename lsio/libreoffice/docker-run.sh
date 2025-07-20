bff3923826d17d64334512b472e26a2e4cccec55
technorabilia <simon@technorabilia.com>
2025-07-20 01:02:48 +0000

    Workflow update

--- lsio/libreoffice/docker-run.sh
+++ lsio/libreoffice/docker-run.sh
@@ -9,7 +9,7 @@ docker run -d \
   -e PUID=${PUID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the UID to use here.` \
   -e PGID=${PGID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the GID to use here.` \
   -e TZ=${TZ:-Etc/UTC} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
-  -p 3000:3000 `# LibreOffice desktop gui.` \
+  -p 3000:3000 `# LibreOffice desktop gui HTTP, must be proxied.` \
   -p 3001:3001 `# LibreOffice desktop gui HTTPS.` \
   -v ${BASEDIR:-/srv/lsio}/libreoffice/config:/config `# Users home directory in the container, stores program settings and documents` \
   --restart unless-stopped \
