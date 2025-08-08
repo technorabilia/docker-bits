586f96b2af5655bda74480d7a1122981c2a64396
technorabilia <simon@technorabilia.com>
2025-08-08 00:59:12 +0000

    Workflow update

--- lsio/sickgear/docker-run.sh
+++ lsio/sickgear/docker-run.sh
@@ -10,7 +10,7 @@ docker run -d \
   -e PGID=${PGID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the GID to use here.` \
   -e TZ=${TZ:-Etc/UTC} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
   -p 8081:8081 `# will map the container's port 8081 to port 8081 on the host` \
-  -v ${BASEDIR:-/srv/lsio}/sickgear/config:/config `# Persistent configuration files.` \
+  -v ${BASEDIR:-/srv/lsio}/sickgear/config:/config `# Persistent config files.` \
   -v ${BASEDIR:-/srv/lsio}/sickgear/tv:/tv `# where you store your tv shows` \
   -v ${BASEDIR:-/srv/lsio}/sickgear/downloads:/downloads `# your downloads folder for post processing (must not be download in progress)` \
   --restart unless-stopped \
