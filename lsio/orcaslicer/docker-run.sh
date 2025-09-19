871a8ee9dc3eadf03f5b140b66e12acaebb59fc5
technorabilia <simon@technorabilia.com>
2025-09-19 00:49:13 +0000

    Workflow update

--- lsio/orcaslicer/docker-run.sh
+++ lsio/orcaslicer/docker-run.sh
@@ -10,8 +10,9 @@ docker run -d \
   -e PUID=${PUID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the UID to use here.` \
   -e PGID=${PGID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the GID to use here.` \
   -e TZ=${TZ:-Etc/UTC} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
-  -p 3000:3000 `# Orca Slicer desktop gui.` \
+  -p 3000:3000 `# Orca Slicer desktop gui HTTP, must be proxied.` \
   -p 3001:3001 `# Orca Slicer desktop gui HTTPS.` \
   -v ${BASEDIR:-/srv/lsio}/orcaslicer/config:/config `# Users home directory in the container, stores program settings and files.` \
+  --shm-size="1gb" `# [OPTIONAL]` \
   --restart unless-stopped \
   lscr.io/linuxserver/orcaslicer:latest
