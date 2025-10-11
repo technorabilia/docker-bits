4d3ee589c6444266d066036e8178212e3870b852
technorabilia <simon@technorabilia.com>
2025-10-11 00:45:29 +0000

    Workflow update

--- lsio/manyfold/docker-run.sh
+++ lsio/manyfold/docker-run.sh
@@ -10,7 +10,7 @@ docker run -d \
   -e TZ=${TZ:-Etc/UTC} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
   -e DATABASE_URL= `# Database connection URL. For sqlite use `sqlite3:/config/manyfold.sqlite3`. For postgres or mariadb use `<scheme>://<username>:<password>@<hostname>:<port>/<db name>` where `<scheme>` is `postgresql` or `mysql2`. Special characters in username/password must be [URL encoded](https://en.wikipedia.org/wiki/Percent-encoding).` \
   -e REDIS_URL= `# Redis/Valkey database URL in `redis://<hostname>:<port>/<db number>` format.` \
-  -e SECRET_KEY_BASE= `# Browser session secret. Changing it will terminate all active browser sessions.` \
+  -e SECRET_KEY_BASE= `# Browser session and database encryption key. If unset a random one will be generated on init, the database is locked to this key.` \
   -p 3214:3214 `# Port for web frontend` \
   -v ${BASEDIR:-/srv/lsio}/manyfold/config:/config `# Persistent storage for application configuration data.` \
   -v ${BASEDIR:-/srv/lsio}/manyfold/libraries:/libraries `# Location of your 3D model libraries. [OPTIONAL]` \
