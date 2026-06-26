29f40e480c1bb53584811f4f86065183b243d112
technorabilia <simon@technorabilia.com>
2026-06-26 03:57:32 +0000

    Workflow update

---
+++ lsio/htpcmanager/docker-run.sh
@@ -0,0 +1,13 @@
+# [Htpcmanager](https://github.com/HTPC-Manager/HTPC-Manager) is a front end for
+# many htpc related applications.
+
+. ./.env
+docker run -d \
+  --name=htpcmanager \
+  -e PUID=${PUID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the UID to use here.` \
+  -e PGID=${PGID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the GID to use here.` \
+  -e TZ=${TZ:-Etc/UTC} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -p 8085:8085 `# Application WebUI` \
+  -v ${BASEDIR:-/srv/lsio}/htpcmanager/config:/config `# Persistent config files` \
+  --restart unless-stopped \
+  lscr.io/linuxserver/htpcmanager:latest
