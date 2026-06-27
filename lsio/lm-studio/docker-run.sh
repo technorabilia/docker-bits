4ca66827d02013593efa3d437d3c81ec1afbc2f6
technorabilia <simon@technorabilia.com>
2026-06-27 03:40:17 +0000

    Workflow update

---
+++ lsio/lm-studio/docker-run.sh
@@ -0,0 +1,15 @@
+# [LM Studio](https://lmstudio.ai/) can run local AI models like gpt-oss, Llama,
+# Gemma, Qwen, and DeepSeek privately on your computer.
+
+. ./.env
+docker run -d \
+  --name=lm-studio \
+  -e PUID=${PUID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the UID to use here.` \
+  -e PGID=${PGID:-1000} `# Run 'id [USER]' for the owner of the host volume directories to get the GID to use here.` \
+  -e TZ=${TZ:-Etc/UTC} `# specify a timezone to use, see this [list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List).` \
+  -p 3000:3000 `# LM Studio desktop gui HTTP, must be proxied.` \
+  -p 3001:3001 `# LM Studio desktop gui HTTPS.` \
+  -v ${BASEDIR:-/srv/lsio}/lm-studio/config:/config `# Users home directory in the container, stores program settings and files.` \
+  --shm-size="1gb" `# [OPTIONAL]` \
+  --restart unless-stopped \
+  lscr.io/linuxserver/lm-studio:latest
